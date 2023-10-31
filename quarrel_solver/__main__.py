65426ea91a70d33035cd9653f189ef96f3271087
silvncr <johnsdieckmann@gmail.com>
2023-11-01 00:31:44 +1000

    Update __main__.py

--- quarrel_solver/__main__.py
+++ quarrel_solver/__main__.py
@@ -1,55 +1,57 @@
-import json, os, string, sys
-from quarrel_solver import Ruleset, build_settings
+import contextlib, json, os, string, sys
+from quarrel_solver import build_settings, Ruleset
 
-print('\n\tloading settings, wordlist..')
+with contextlib.suppress(KeyboardInterrupt):
 
-try:
-	with open(
-		os.path.join(
-			sys.path[0], 'settings.json'
-		), 'tr'
-	) as settings_file:
-		settings_import = json.load(settings_file)
+	print('\n\tloading settings, wordlist..')
 
-except FileNotFoundError:
-	settings_import = {}
+	try:
+		with open(
+			os.path.join(
+				sys.path[0], 'settings.json'
+			), 'tr'
+		) as settings_file:
+			settings_import = json.load(settings_file)
 
-	print('\n\tcould not load settings.json! continuing with default settings..')
+	except FileNotFoundError:
+		settings_import = {}
 
-q = Ruleset(
-	settings=build_settings(settings_import)
-)
+		print('\n\tcould not load settings.json! continuing with default settings..')
 
-if q['display_debug']:
-	print(f'\n{q.get_settings()}\n')
-
-try:
-	open(
-		os.path.join(
-			sys.path[0], 'settings.json'
-		), 'tw'
-	).write(
-		q.get_settings_str()
+	q = Ruleset(
+		settings=build_settings(settings_import)
 	)
 
-except Exception:
-	print('\n\tcould not save settings.json! continuing..')
+	if q['display_debug']:
+		print(f'\n{q.get_settings()}\n')
 
-print('\n\t\tdone!')
+	try:
+		open(
+			os.path.join(
+				sys.path[0], 'settings.json'
+			), 'tw'
+		).write(
+			q.get_settings_str()
+		)
 
-while True:
-	print('')
-	query = ''
-	while not all([
-		q['min_words_len'] <= len(query) <= q['max_words_len'],
-		all(char not in string.ascii_lowercase for char in query)
-	]):
-		print(f'\n{
-			q.solve_str(
-				''.join(
-					sorted(dict.fromkeys(input('> ').lower()))
-					if q['allow_repeats']
-					else sorted(input('> ').lower())
+	except Exception:
+		print('\n\tcould not save settings.json! continuing..')
+
+	print('\n\t\tdone!')
+
+	while True:
+		print('')
+		query = ''
+		while not all([
+			q['min_words_len'] <= len(query) <= q['max_words_len'],
+			all(char not in string.ascii_lowercase for char in query)
+		]):
+			print(f'\n{
+				q.solve_str(
+					''.join(
+						sorted(dict.fromkeys(input('> ').lower()))
+						if q['allow_repeats']
+						else sorted(input('> ').lower())
+					)
 				)
-			)
-		}\n')
+			}\n')
